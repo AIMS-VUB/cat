5e433ddb75897033d8e0862f47749ac8bb98ac7a
jlaton <jorne.laton@gmail.com>
2023-06-12 18:06:48 +0200

    Add support to compare bandpower to a baseline Add percentage bandpower to output

--- Channel Analysis Toolbox/spect/cat_spect_bandpower.m
+++ Channel Analysis Toolbox/spect/cat_spect_bandpower.m
@@ -1,4 +1,4 @@
-function E = cat_spect_bandpower(E, bands, bandlabels)
+function E = cat_spect_bandpower(E, bands, bandlabels, baseline)
 %CAT_SPECT_BANDPOWER - Power in different bands
 %
 %   Calculates the spectral power in different bands
@@ -10,15 +10,16 @@ function E = cat_spect_bandpower(E, bands, bandlabels)
 %   bands is a 2D matrix containing frequency intervals in its rows for
 %         which the power must be calculated
 %   bandlabels is a cell array containing the band labels, e.g. 'alpha'
+%   baseline   is an optional index to select a band to subtract from every other
 %
 %   See also INTERVALMEAN, CAT_SPECT
 
-%   #2018.11.28 Jorne Laton#
+%   #2023.05.05 Jorne Laton#
 
 data_spec_squared = E.spect.average.^2;
 n_bands = size(bands, 1);
 E.spect.bandpower = [];
-E.spect.bandpower.average = zeros(size(E.spect.average, 1), n_bands, size(E.spect.average, 3));
+E.spect.bandpower.average = zeros(size(E.spect.average, 2), n_bands, size(E.spect.average, 3));
 E.spect.bandpower.bands = bands;
 E.spect.bandpower.labels = bandlabels;
 
@@ -26,5 +27,16 @@ for i = 1 : n_bands
   E.spect.bandpower.average(:, i, :) = intervalmean(E.spect.freqs, data_spec_squared, bands(i, :));
 end
 
+if (nargin > 3)
+    for i = 1 : n_bands
+        if (i == baseline)
+            continue
+        end
+        E.spect.bandpower.average(:, i, :) = E.spect.bandpower.average(:, i, :) - E.spect.bandpower.average(:, baseline, :);
+    end
+    E.spect.bandpower.baseline = bandlabels(baseline);
 end
 
+E.spect.bandpower.percentage = E.spect.bandpower.average ./ repmat(sum(E.spect.bandpower.average, 2), 1, n_bands);
+
+end
